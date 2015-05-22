839a3ab4a3e4f7c340d6cd93d4a2cc4068b89f6c
Jorge del Casar <jorge.casar@gmail.com>
2015-05-22 23:43:34 +0200

    Improved add Happiness page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,39 +5,29 @@ angular.module('app.controllers', [])
 .controller('TrendingCtrl', function($scope, $ionicModal, Happinesses) {
   $scope.happinesses = [];
   $scope.happinessRange = [1, 2, 3, 4, 5];
+  $scope.happinessRangeMin = $scope.happinessRange[0];
+  $scope.happinessRangeMax = $scope.happinessRange[$scope.happinessRange.length - 1];
+
   var happinesses = Happinesses.query(function() {
     $scope.happinesses = happinesses;
   });
   
-  $scope.addHappiness = 
-  
-  $scope.happinessData = {};
-
   // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/addHappiness.html', {
+  $ionicModal.fromTemplateUrl('templates/addHappinessModal.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
   });
 
-  // Triggered in the login modal to close it
+  // Triggered in the modal to close it
   $scope.closeAddHappiness = function() {
     $scope.modal.hide();
   };
 
-  // Open the login modal
+  // Open the modal
   $scope.openAddHappiness = function() {
     $scope.modal.show();
   };
-
-  // Perform the login action when the user submits the login form
-  $scope.addHappiness = function() {
-    Happinesses.save($scope.happinessData, function(newHappiness){
-      $scope.happinesses.unshift(newHappiness);
-      $scope.closeAddHappiness();
-    });
-  };
-
 })
 
 .controller('AboutCtrl', function($scope) {
---
+++ www/js/directives.js
@@ -0,0 +1,49 @@
+angular.module('app.directives', [])
+.directive('hackhappinesAddhackhappines', function(Happinesses) {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'templates/addHappiness.html',
+      compile: function(element, attr) {
+        return function($scope)
+        {
+          // Perform the action when the user submits the login form
+          $scope.addHappiness = function() {
+            Happinesses.save($scope.happinessData, function(newHappiness){
+              $scope.happinesses.unshift(newHappiness);
+              $scope.closeAddHappiness();
+            });
+          };
+          
+          var interval;
+          $scope.startHappiness = function()
+          {
+            _increaseHappinessLevel();
+            interval = setInterval(_increaseHappinessLevel, _increaseHappinessInterval);
+          };
+          $scope.stopHappiness = function()
+          {
+            clearInterval(interval);
+            $scope.isHappinessLevelSet = true;
+          };
+
+          var _increaseHappinessInterval = 500;
+          var _increaseHappinessLevel = function(){
+            if( $scope.happinessData.level < $scope.happinessRangeMax )
+            {
+              $scope.happinessData.level++;
+            }
+          };
+          $scope.initHappinesData = function()
+          {
+            $scope.happinessData = {
+              level: 0,
+              message: ''
+            };
+            $scope.isHappinessLevelSet = false;
+          };
+          $scope.initHappinesData();
+        };
+      }
+    };
+});
